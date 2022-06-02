eebae8ba78ff0614c62525062a47832339b8b67d
Grzegorz Kołakowski <grzegorz.kolakowski@getindata.com>
2022-06-02 14:12:10 +0200

    WIP

--- benchmarks/infrastructure/aws/terraform/outputs.tf
+++ benchmarks/infrastructure/aws/terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "master_node_address" {
-  value = aws_emr_cluster.benchmarks.master_public_dns
+#output "master_node_address" {
+#  value = aws_emr_cluster.benchmarks.master_public_dns
+#}
+
+output "load_balancer_address" {
+  value = aws_alb.default.dns_name
 }
---
+++ benchmarks/infrastructure/aws/terraform/user_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo ECS_CLUSTER=flink-session-cluster >> /etc/ecs/ecs.config
